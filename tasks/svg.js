5a8d12d777cc3efb842702aaf01eea9fdf9dd91e
Rob Lindsey <rclinds@icloud.com>
2018-03-19 18:26:39 -0400

    Working on images and CSS

--- tasks/svg.js
+++ tasks/svg.js
@@ -1,11 +1,17 @@
 import gulp from 'gulp';
 import paths from './paths';
 
-const sprites = require('gulp-svg-sprites');
+const sprites = require('gulp-svg-sprite');
+
+const config = {
+  mode: {
+    symbol: true,
+  },
+};
 
 export const svgSpriteSheet = gulp.task('svgSpriteSheet', (done) => {
   gulp.src(paths.svg.src)
-    .pipe(sprites({ mode: 'symbols' }))
+    .pipe(sprites(config))
     .pipe(gulp.dest(paths.svg.dest));
   done();
 });
