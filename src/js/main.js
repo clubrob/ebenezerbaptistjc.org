88bd37d02f735572170f14787fed17f69a9e9f34
Rob Lindsey <rclinds@icloud.com>
2018-03-21 19:16:06 -0400

    Debugging

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-$('a[href^="#"]').on('click', function scrolly(event) {
-  const target = $(this.getAttribute('href'));
-  if (target.length) {
-    event.preventDefault();
-    $('html, body').stop().animate({
-      scrollTop: target.offset().top,
-    }, 800);
-  }
-});
+function payload() {
+  $('a[href^="#"]').on('click', function scrolly(event) {
+    const target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top,
+      }, 800);
+    }
+  });
+}
+
+$(document).ready(payload);
--- tasks/script.js
+++ tasks/script.js
@@ -1,10 +1,14 @@
 import gulp from 'gulp';
+import babel from 'gulp-babel';
 import paths from './paths';
 
 const concat = require('gulp-concat');
 
 const compileScript = gulp.task('compileScript', () =>
   gulp.src(paths.scripts.src)
+    .pipe(babel({
+      presets: ['env'],
+    }))
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest(paths.scripts.dest)));
 
